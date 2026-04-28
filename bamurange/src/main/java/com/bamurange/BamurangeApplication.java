ba1c4f8ffecac70163a0b01d8eb8fd8ccdcb6a0f
sarahbamurange <sarahbamy929@gmail.com>
2026-04-28 22:35:18 +0200

    done the project

---
+++ bamurange/src/main/java/com/bamurange/BamurangeApplication.java
@@ -0,0 +1,13 @@
+package com.bamurange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamurangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BamurangeApplication.class, args);
+	}
+
+}
---
+++ bamurange/src/test/java/com/bamurange/BamurangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamurange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamurangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
